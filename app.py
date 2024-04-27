04eb855683da0a9097d4790d0eba7d5762d2641e
Shamit Mishra <82385464+iamshamit@users.noreply.github.com>
2024-04-27 21:51:09 +0000

    Good Progress

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , jsonify
 
 app = Flask(__name__)
 
+parts = [
+  {
+    'id': 1,
+    'name': 'CPU',
+    'price': "Rs 1000",
+    'quant': 1,
+    'image': 'static/cpu.jpg'
+  },
+  {
+    'id': 2,
+    'name': 'GPU',
+    'price': "Rs 2000",
+    'quant' : 1,
+    'image': 'static/gpu.jpg'
+  },
+  {
+    'id': 3,
+    'name': 'RAM',
+    'price': "Rs 1000",
+    'quant': 1,
+    'image': 'static/ram.jpg'
+  },
+  {
+    'id': 4,
+    'name': 'SSD',
+    'price': "Rs 3000",
+    'quant': 1,
+    'image': 'static/ssd.jpg'
+  }
+]
+
 @app.route("/")
 def hello_ewastex():
-    return render_template("home.html")
+    return render_template("home.html",Parts=parts)
+
+@app.route("/api/parts")
+def list_parts():
+    return jsonify(parts)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
